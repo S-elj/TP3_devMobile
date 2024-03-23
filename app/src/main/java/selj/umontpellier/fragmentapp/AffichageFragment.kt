75b3efa32cdecc07e6354f4da9140018b49d120e
Selj <samy.el-jaafari@etu.umontpellier.fr>
2024-03-23 16:13:19 +0100

    feat(exo 2) : save les données dans in fichier interne

--- app/src/main/java/selj/umontpellier/fragmentapp/AffichageFragment.kt
+++ app/src/main/java/selj/umontpellier/fragmentapp/AffichageFragment.kt
@@ -1,11 +1,15 @@
 package selj.umontpellier.fragmentapp
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import org.json.JSONObject
+import java.io.IOException
 
 class AffichageFragment : androidx.fragment.app.Fragment() {
 
@@ -28,15 +32,45 @@ class AffichageFragment : androidx.fragment.app.Fragment() {
             view.findViewById<TextView>(R.id.tvTel).text = bundle.getString("tel", "")
             view.findViewById<TextView>(R.id.tvMail).text = bundle.getString("mail", "")
             view.findViewById<TextView>(R.id.tvCentreInteret).text = bundle.getString("centresInteret", "")
-            // Répétez pour les autres données si
-        }
 
-        view.findViewById<Button>(R.id.btnRetour).setOnClickListener {
+            val jsonSaver = JSONObject().apply {
+                put("nom", bundle.getString("nom", ""))
+                put("prenom", bundle.getString("prenom", ""))
+                put("birth", bundle.getString("birth", ""))
+                put("tel", bundle.getString("tel", ""))
+                put("mail", bundle.getString("mail", ""))
+                put("centresInteret", bundle.getString("centresInteret", ""))
+                // Ajoutez les autres champs ici
+
+            }
+
+            view.findViewById<Button>(R.id.btnValider).setOnClickListener {
+
+                view.findViewById<Button>(R.id.btnValider).setOnClickListener {
+                    try {
+                        val filename = "userData.json"
+                        val fileOutput = context?.openFileOutput(filename, Context.MODE_PRIVATE)
+                        fileOutput?.write(jsonSaver.toString().toByteArray())
+                        fileOutput?.close()
+
+                        Toast.makeText(context, "Données sauvegardées", Toast.LENGTH_SHORT).show()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                        Toast.makeText(context, "Erreur lors de la sauvegarde", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+
+            }
+
+
+            view.findViewById<Button>(R.id.btnRetour).setOnClickListener {
                 val formFragment = SaisieFragment()
                 fragmentManager?.beginTransaction()
-                ?.replace(R.id.fragment_container, formFragment)
-                ?.commit()
+                    ?.replace(R.id.fragment_container, formFragment)
+                    ?.commit()
+            }
         }
 
-        }
+    }
 }
